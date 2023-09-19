70bbe41520995d02327ca4d8ed072aceb4ef5a82
zhangchunsheng <zhangchunsheng423@gmail.com>
2023-09-19 10:28:11 +0800

    update

--- scripts/make_docs.sh
+++ scripts/make_docs.sh
@@ -22,7 +22,7 @@ cp -a "${docs_folder}/css/"* "${raw_folder}"
 cp -a "${docs_folder}/images/"* "${raw_folder}"
 cp -a "${docs_folder}/en/index.html" "${raw_folder}/"
 echo en
-sed -i -e 's|src=\.\./images/|src=|' \
+gsed -i -e 's|src=\.\./images/|src=|' \
  -e 's|href=\.\./css/|href=|' \
  -e 's|data=\.\./images/|data=|'  "${raw_folder}/index.html"
 echo en
@@ -33,7 +33,7 @@ for lang in "${SUPPORTED_LANGUAGES[@]}"; do
   [[ -d "${raw_folder}" ]] || mkdir -p "${raw_folder}"
   cp -a "${docs_folder}/${lang}/index.html" "${raw_folder}/"
   echo ${raw_folder}
-  sed -i -e 's|src=\.\./images/|src=|' \
+  gsed -i -e 's|src=\.\./images/|src=|' \
    -e 's|href=\.\./css/|href=|' \
    -e 's|data=\.\./images/|data=|' "${raw_folder}/index.html"
   echo ${raw_folder}
